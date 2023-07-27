b367375fa4f40c9b4db35c2db521884b6c3a5a79
Chaitanya Ramekar <ramekarc@gmail.com>
2023-07-27 17:15:39 +0530

    completed

--- src/GUI/AppFrame.java
+++ src/GUI/AppFrame.java
@@ -1,9 +1,13 @@
 package src.GUI;
 
+import src.COMP_DECOMP.compressor;
+import src.COMP_DECOMP.decompressor;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 
 public class AppFrame extends JFrame implements ActionListener {
@@ -12,17 +16,50 @@ public class AppFrame extends JFrame implements ActionListener {
 
     AppFrame() {
         this.setDefaultCloseOperation(3);
-        this.compressButton = new JButton("Compress");
-        this.compressButton.setBounds(20, 100, 200, 30);
-        this.decompressButton = new JButton("Decompress");
-        this.decompressButton.setBounds(250, 200, 200, 30);
-        this.add(this.compressButton);
-        this.add(this.decompressButton);
-        this.setSize(100, 500);
+
+        compressButton = new JButton("Compress");
+        compressButton.setBounds(20, 100, 150, 30);
+        compressButton.addActionListener(this);
+
+        decompressButton = new JButton("Decompress");
+        decompressButton.setBounds(250, 100, 150, 30);
+        decompressButton.addActionListener(this);
+
+        this.add(compressButton);
+        this.add(decompressButton);
+        this.setSize(1000, 500);
         this.getContentPane().setBackground(Color.CYAN);
         this.setVisible(true);
     }
-
+    @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==compressButton){
+            JFileChooser filechooser=new JFileChooser();
+            int response=filechooser.showOpenDialog(null);
+            if (response==JFileChooser.APPROVE_OPTION){
+                File file=new File(filechooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+                try{
+                    compressor.method(file);
+                }
+                catch (Exception ee){
+                    JOptionPane.showMessageDialog(null,ee.toString());
+                }
+            }
+        }
+        if(e.getSource()==decompressButton){
+            JFileChooser filechooser=new JFileChooser();
+            int response=filechooser.showOpenDialog(null);
+            if (response==JFileChooser.APPROVE_OPTION){
+                File file=new File(filechooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+                try{
+                    decompressor.method(file);
+                }
+                catch (Exception ee){
+                    JOptionPane.showMessageDialog(null,ee.toString());
+                }
+            }
+        }
     }
 }
--- src/GUI/main.java
+++ src/GUI/main.java
@@ -1,4 +1,7 @@
 package src.GUI;
 
 public class main {
+    public static void main(String[] args){
+        AppFrame frame=new AppFrame();
+    }
 }
